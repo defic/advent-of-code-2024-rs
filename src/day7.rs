21a45d9aad28b554d4eaf3140a3be12c7976aed2
Jeremias <jerccu@gmail.com>
2024-12-07 18:00:15 +0200

    Using recursion for day7 task2

--- src/day7.rs
+++ src/day7.rs
@@ -31,41 +31,64 @@ fn task1(input: String) -> String {
 
 fn task2(input: String) -> String {
     let problems = parse_input(input);
+    let operators = &[Operator::Add, Operator::Multiply, Operator::Concat];
+    problems
+        .iter()
+        .filter(|(goal, members)| has_solution2(operators, *goal, members[0], &members[1..]))
+        .map(|c| c.0)
+        .sum::<u64>()
+        .to_string()
+}
 
-    let mut total = 0;
-    for (answer, mut members) in problems {
-        let mut accs: Vec<u64> = vec![members.remove(0)];
-
-        for member in members {
-            accs = accs
-                .iter()
-                .flat_map(|acc| vec![acc * member, acc + member, combine(*acc, member)])
-                .collect()
-        }
-        if accs.contains(&answer) {
-            total += answer;
-        }
+fn has_solution2(operators: &[Operator], goal: u64, current: u64, rest: &[u64]) -> bool {
+    if rest.is_empty() {
+        return goal == current;
     }
 
-    total.to_string()
+    if current > goal {
+        return false;
+    }
+
+    let first = rest.first().unwrap();
+
+    operators
+        .iter()
+        .any(|op| has_solution2(operators, goal, op.calc(current, *first), &rest[1..]))
+}
+
+enum Operator {
+    Multiply,
+    Add,
+    Concat,
+}
+
+impl Operator {
+    fn calc(&self, a: u64, b: u64) -> u64 {
+        match self {
+            Operator::Add => a + b,
+            Operator::Multiply => a * b,
+            Operator::Concat => concat(a, b),
+        }
+    }
 }
 
 fn parse_input(input: String) -> Vec<(u64, Vec<u64>)> {
     let problems: Vec<_> = input
         .lines()
         .map(|line| {
-            let mut split = line.split(":");
-            let answer = split.next().unwrap().parse::<u64>().unwrap();
-            let mut members = split.next().unwrap().to_string();
-            members.remove(0); //space
-            let members: Vec<_> = members.split(" ").flat_map(|e| e.parse::<u64>()).collect();
+            let (answer, rest) = line.split_once(":").unwrap();
+            let answer = answer.parse::<u64>().unwrap();
+            let members: Vec<_> = rest
+                .split_ascii_whitespace()
+                .map(|x| x.parse::<u64>().unwrap())
+                .collect();
             (answer, members)
         })
         .collect();
     problems
 }
 
-fn combine(num1: u64, num2: u64) -> u64 {
+fn concat(num1: u64, num2: u64) -> u64 {
     let num2_len = count_digits(num2);
     let multiplier = 10_u64.pow(num2_len);
     num1 * multiplier + num2
